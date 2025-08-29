f654aaff6a975d8d9a0fe5f14bd993edbecf6910
Mykyta Maltsev <n.maltsev.dis@gmail.com>
2025-08-29 23:22:03 +0300

    add type id implementation details

--- src/core/type_id.hpp
+++ src/core/type_id.hpp
@@ -7,4 +7,8 @@ namespace he
 
 using type_id_t = uint32_t;
 
+// type id cannot be a function-local static variable, because multiple template instantiations
+// in different libraries would result in duplicates. Use type name instead. Each major compiler
+// has it's own function signature format (std::source_location), parsers should be wirtten
+// accordingly
 }
