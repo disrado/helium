169a3be176955b8ccada9d0c67a5d32e3852dc70
Mykyta Maltsev <n.maltsev.dis@gmail.com>
2025-08-30 23:49:20 +0300

    add type_name prototype

--- src/core/type_id.hpp
+++ src/core/type_id.hpp
@@ -11,4 +11,10 @@ using type_id_t = uint32_t;
 // in different libraries would result in duplicates. Use type name instead. Each major compiler
 // has it's own function signature format (std::source_location), parsers should be wirtten
 // accordingly
+
+// provide functionality to get type name. Should be parsed from function signature
+template<typename T>
+auto type_name() -> std::string
+{
+}
 }
