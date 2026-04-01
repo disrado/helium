676016642b70b3b217932c83e1291d82291d72d2
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-04-01 23:58:28 +0300

    test for loggin system

---
+++ src/game/systems/test/log_system_test.cpp
@@ -0,0 +1,9 @@
+﻿#include "game/systems/log_system.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+
+TEST_CASE("type id")
+{
+    ne::info{ "tag", "format: {}", "message", 10, std::string{} };
+}
