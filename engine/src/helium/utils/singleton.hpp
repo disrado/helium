0d5cabf84d88fb3fdf75d91bf6449728d189c384
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-04-19 23:55:43 +0300

    implement base singleton

---
+++ engine/src/helium/utils/singleton.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+
+namespace he
+{
+
+template <typename t>
+class singleton
+{
+public:
+    virtual ~singleton() = default;
+
+protected:
+    singleton() = default;
+
+public:
+    static auto instance() -> t&
+    {
+        static auto object_instance{ t{} };
+        return object_instance;
+    }
+};
+
+}
