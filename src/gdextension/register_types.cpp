fc6839a70cd480e1e94772e404e8c36e12be474d
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-17 23:26:42 +0300

    fix code style

--- src/gdextension/register_types.cpp
+++ src/gdextension/register_types.cpp
@@ -8,6 +8,7 @@
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 
+
 using namespace godot;
 
 void initialize_helium_module(ModuleInitializationLevel p_level)
--- src/gdextension/register_types.h
+++ src/gdextension/register_types.h
@@ -2,6 +2,7 @@
 
 #include <godot_cpp/core/class_db.hpp>
 
+
 using namespace godot;
 
 void initialize_helium_module(ModuleInitializationLevel p_level);
