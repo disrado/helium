208f720f98c8bf766cc0d75f754428191004d07c
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-20 00:28:18 +0300

    fix cmake var names, project name and build type propagation

--- deps/godot.cmake
+++ deps/godot.cmake
@@ -26,12 +26,14 @@ if(SCONS_EXECUTABLE)
         USES_TERMINAL VERBATIM)
 
     add_custom_target(godot_game_debug
-        COMMAND ${SCONS_EXECUTABLE} ${HELIUM_GODOT_COMMON_ARGS} target=template_debug extra_suffix=.helium-debug
+        COMMAND ${SCONS_EXECUTABLE} ${HELIUM_GODOT_COMMON_ARGS} target=template_debug
+            extra_suffix=.${CMAKE_PROJECT_NAME}-debug disable_path_overrides=no
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/deps/godot
         USES_TERMINAL VERBATIM)
 
     add_custom_target(godot_game_release
-        COMMAND ${SCONS_EXECUTABLE} ${HELIUM_GODOT_COMMON_ARGS} target=template_release extra_suffix=.helium-release
+        COMMAND ${SCONS_EXECUTABLE} ${HELIUM_GODOT_COMMON_ARGS} target=template_release
+            extra_suffix=.${CMAKE_PROJECT_NAME}-release disable_path_overrides=no
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/deps/godot
         USES_TERMINAL VERBATIM)
 endif()
--- test/game_test.cmake
+++ test/game_test.cmake
@@ -1,26 +1,26 @@
 if(WIN32)
-    set(HELIUM_GAME_BIN_EXT ".exe")
+    set(GAME_BIN_EXT ".exe")
 else()
-    set(HELIUM_GAME_BIN_EXT "")
+    set(GAME_BIN_EXT "")
 endif()
 
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(HELIUM_GAME_SUFFIX "helium-debug")
-else()
-    set(HELIUM_GAME_SUFFIX "helium-release")
-endif()
+set(GAME_SUFFIX "${CMAKE_PROJECT_NAME}-${HELIUM_BUILD_TYPE}")
 
-file(GLOB HELIUM_GAME_BIN_CANDIDATES ${CMAKE_SOURCE_DIR}/deps/godot/bin/*.${HELIUM_GAME_SUFFIX}${HELIUM_GAME_BIN_EXT})
-list(LENGTH HELIUM_GAME_BIN_CANDIDATES HELIUM_GAME_BIN_COUNT)
+file(GLOB GAME_BIN_CANDIDATES ${CMAKE_SOURCE_DIR}/deps/godot/bin/*.${GAME_SUFFIX}${GAME_BIN_EXT})
+list(LENGTH GAME_BIN_CANDIDATES GAME_BIN_COUNT)
 
-if(HELIUM_GAME_BIN_COUNT EQUAL 1)
-    list(GET HELIUM_GAME_BIN_CANDIDATES 0 HELIUM_GAME_BIN)
+if(GAME_BIN_COUNT EQUAL 1)
+    list(GET GAME_BIN_CANDIDATES 0 GAME_BIN)
+
     add_test(NAME game_startup
-        COMMAND ${HELIUM_GAME_BIN} --path ${CMAKE_SOURCE_DIR} --headless --quit
+        COMMAND ${GAME_BIN} --path ${CMAKE_SOURCE_DIR} --headless --quit
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+
     set_tests_properties(game_startup PROPERTIES
         TIMEOUT 60
         FAIL_REGULAR_EXPRESSION "SCRIPT ERROR;ERROR:")
-elseif(HELIUM_GAME_BIN_COUNT GREATER 1)
-    message(WARNING "Multiple godot_game binaries match *.${HELIUM_GAME_SUFFIX}${HELIUM_GAME_BIN_EXT} in deps/godot/bin - game_startup test not registered, clean up stale builds")
+elseif(GAME_BIN_COUNT GREATER 1)
+    message(WARNING
+        "Multiple godot_game binaries match *.${GAME_SUFFIX}${GAME_BIN_EXT} in deps/godot/bin - "
+        "game_startup test not registered, clean up stale builds")
 endif()
