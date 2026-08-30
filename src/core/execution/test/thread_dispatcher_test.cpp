0eb2f83afe7c30f6f1aba1683e4e0881dbffa929
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-30 03:00:55 +0300

    cover thread_dispatcher with tests

--- src/core/execution/test/thread_dispatcher_test.cpp
+++ src/core/execution/test/thread_dispatcher_test.cpp
@@ -2,7 +2,80 @@
 
 #include <catch2/catch_test_macros.hpp>
 
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+
 
 TEST_CASE("thread_dispatcher")
 {
+    SECTION("runs work")
+    {
+        auto instance{ he::exec::thread_dispatcher{} };
+
+        auto mutex{ std::mutex{} };
+        auto cv{ std::condition_variable{} };
+        auto ran{ false };
+
+        instance.dispatch(
+            [&]
+            {
+                const auto _{ std::lock_guard{ mutex } };
+                ran = true;
+                cv.notify_one();
+            });
+
+        auto lock{ std::unique_lock{ mutex } };
+        cv.wait(lock, [&] { return ran; });
+
+        REQUIRE(ran);
+    }
+
+    SECTION("runs all work")
+    {
+        auto instance{ he::exec::thread_dispatcher{} };
+
+        auto mutex{ std::mutex{} };
+        auto cv{ std::condition_variable{} };
+        auto completed{ 0 };
+        constexpr auto work_count{ 20 };
+
+        for (auto i{ 0 }; i < work_count; ++i)
+        {
+            instance.dispatch(
+                [&]
+                {
+                    const auto _{ std::lock_guard{ mutex } };
+                    ++completed;
+                    cv.notify_one();
+                });
+        }
+
+        auto lock{ std::unique_lock{ mutex } };
+        cv.wait(lock, [&] { return completed == work_count; });
+
+        REQUIRE(completed == work_count);
+    }
+
+    SECTION("dispatches through base pointer")
+    {
+        auto instance{ std::unique_ptr<he::exec::dispatcher>{ std::make_unique<he::exec::thread_dispatcher>() } };
+
+        auto mutex{ std::mutex{} };
+        auto cv{ std::condition_variable{} };
+        auto ran{ false };
+
+        instance->dispatch(
+            [&]
+            {
+                const auto _{ std::lock_guard{ mutex } };
+                ran = true;
+                cv.notify_one();
+            });
+
+        auto lock{ std::unique_lock{ mutex } };
+        cv.wait(lock, [&] { return ran; });
+
+        REQUIRE(ran);
+    }
 }
