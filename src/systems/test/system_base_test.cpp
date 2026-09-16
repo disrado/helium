b947abdd7061932cecf2672321627542992645b1
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-09-16 23:52:38 +0300

    setup tests for systems

---
+++ src/systems/test/system_base_test.cpp
@@ -0,0 +1,8 @@
+#include "systems/system_base.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+
+TEST_CASE("system_base")
+{
+}
---
+++ src/systems/test/system_tree_test.cpp
@@ -0,0 +1,8 @@
+#include "systems/system_tree.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+
+TEST_CASE("system_tree")
+{
+}
